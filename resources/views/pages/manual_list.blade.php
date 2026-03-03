ec5c2340d3de63d7538871fa18fea471781126f2
Alaa Aldeen Omaren <d302707@edu.curio.nl>
2026-03-03 14:32:45 +0100

    gebruikt grid i.p.v. list (merk pagina's)

--- resources/views/pages/manual_list.blade.php
+++ resources/views/pages/manual_list.blade.php
@@ -5,25 +5,35 @@
     </x-slot:head>
 
     <x-slot:breadcrumb>
-        <li><a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/" alt="Manuals for '{{$brand->name}}'" title="Manuals for '{{$brand->name}}'">{{ $brand->name }}</a></li>
+        <li><a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/"
+                alt="Manuals for '{{ $brand->name }}'" title="Manuals for '{{ $brand->name }}'">{{ $brand->name }}</a>
+        </li>
     </x-slot:breadcrumb>
 
 
     <h1>{{ $brand->name }}</h1>
 
-    <p>{{ __('introduction_texts.type_list', ['brand'=>$brand->name]) }}</p>
+    <p>{{ __('introduction_texts.type_list', ['brand' => $brand->name]) }}</p>
 
 
+    <div class="row">
         @foreach ($manuals as $manual)
-
-            @if ($manual->locally_available)
-                <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/{{ $manual->id }}/" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
-                ({{$manual->filesize_human_readable}})
-            @else
-                <a href="{{ $manual->url }}" target="new" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
-            @endif
-
-            <br />
+            <div class="col-6 col-md-4 col-lg-3 mb-2">
+                @if ($manual->locally_available)
+                    <a class="d-block p-2 border rounded text-decoration-none"
+                        href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/{{ $manual->id }}/"
+                        title="{{ $manual->name }}">
+                        {{ $manual->name }}
+                        <span class="text-muted small">({{ $manual->filesize_human_readable }})</span>
+                    </a>
+                @else
+                    <a class="d-block p-2 border rounded text-decoration-none" href="{{ $manual->url }}"
+                        target="_blank" title="{{ $manual->name }}">
+                        {{ $manual->name }}
+                    </a>
+                @endif
+            </div>
         @endforeach
+    </div>
 
 </x-layouts.app>
