0c292b4b36a834f47a10c1d2f95776f78fa1b635
Alaa Aldeen Omaren <d302707@edu.curio.nl>
2026-03-12 14:12:56 +0100

    Foudmelding tonen contact.php

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,9 +2,19 @@
 
     <x-slot:title>Contact</x-slot:title>
 
-    @if(session('success'))
+    @if (session('success'))
         <div class="alert alert-success">
             {{ session('success') }}
+        </div>
+    @endif
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul class="mb-0">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
     @endif
 
@@ -13,17 +23,17 @@
 
         <div class="mb-3">
             <label>Name</label>
-            <input type="text" name="name" class="form-control">
+            <input type="text" name="name" class="form-control" value="{{ old('name') }}">
         </div>
 
         <div class="mb-3">
             <label>Email</label>
-            <input type="email" name="email" class="form-control">
+            <input type="email" name="email" class="form-control" value="{{ old('email') }}">
         </div>
 
         <div class="mb-3">
             <label>Message</label>
-            <textarea name="message" class="form-control"></textarea>
+            <textarea name="message" class="form-control">{{ old('message') }}</textarea>
         </div>
 
         <button type="submit" class="btn btn-primary">
