6bf3757e9146311f54159ebed3ef8e7e5526a60d
BoredxManiac <Manicalsome.1@gmail.com>
2026-03-18 09:36:39 +0100

    category_id added

--- database/migrations/2026_03_17_133914_add_category_id_to_manuals_table.php
+++ database/migrations/2026_03_17_133914_add_category_id_to_manuals_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('manuals', function (Blueprint $table) {
-            //
+              $table->integer('category_id')->nullable();
         });
     }
 
